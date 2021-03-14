bb0f2113cde1f09e015ed86c632dc24d675296e1
thomasgoldberger <thomas.goldberger@gmail.com>
2021-03-14 16:24:25 +0100

    changed button color

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title, onAdd, showAdd }) => {
         <header className ='header'>
             <h1>{title}</h1>
             <Button 
-                color={showAdd ? 'red' : 'darkCyan'}
+                color={showAdd ? 'darkred' : 'darkCyan'}
                 text={showAdd ? 'Close' : 'Add'}
                 onClick = {onAdd} />
         </header>
